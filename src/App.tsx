92ef8d19e346173174722c98aeb8abe00c53815c
MAHABUB-ALAM <mahabubalom72091@gmail.com>
2026-06-02 18:59:24 +0300

    added some pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
-import { Button } from "./components/ui/button";
+import { Outlet } from "react-router";
 
 function App() {
   return (
     <div className="flex min-h-svh flex-col items-center justify-center">
-      <Button>Click me</Button>
+      <p>This is the home page.</p>
+      <Outlet />
     </div>
   );
 }
